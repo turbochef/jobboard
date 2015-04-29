c8623ccc1369df7085d966aa273a8ad0453b87ba
Daniel R. Soetanto <turbochef.ds@gmail.com>
2015-04-29 13:51:46 -0500

    added employer devise views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+	before_action :authenticate_student!
+
   def home
   	@jobs = Job.all.order
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :employers
   resources :jobs
 
-  root 'pages/home'
+  root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
